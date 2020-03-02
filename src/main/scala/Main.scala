330cb02b44415b22d61b4161774cce3643c6ac8c
Chrisrea646 <christopher.rea@academytrainee.com>
2020-03-02 15:05:37 +0000

    notes

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,17 @@
 object Main extends App {
-  println("Hello World")
+
+  println("Hello World!")
+
+  val name = "Chris"
+  //name = "George" -- VAL cant be changed, hense this wont work
+
+  var car = "Ford"
+  car = "Audi"
+  //this will work as var is mutable(can be changed)
+
+  //methods rely on the values within an object
+  //functions will perform the same thing on whatever is inputted i.e a doubling function
+
+
 
 }
